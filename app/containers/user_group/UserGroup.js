d56f2b8ec632ec11aff386e832a8458df5fc99cc
que2018 <goodiswill2016@gmail.com>
2019-08-26 22:10:45 -0700

    08262019

--- app/containers/user_group/UserGroup.js
+++ app/containers/user_group/UserGroup.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react'
 import {Switch, Route} from 'react-router-dom'
-
-import UserGroupList from "./user_list/UserGroupList"
+import UserGroupList from "./user_group_list/UserGroupList"
 
 export default class UserGroup extends Component {
 
--- app/containers/user_group/user_group_list/UserGroupList.js
+++ app/containers/user_group/user_group_list/UserGroupList.js
@@ -2,9 +2,10 @@ import React, {Component, PropTypes} from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
 import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
-import {Table, Pagination} from 'antd';
-import {actions} from '../../reducers/user_group/user_group_list'
-import {UserGroupCell} from "./components/UserGroupCell";
+import {Table, Row, Col, Pagination} from 'antd';
+import {Button} from 'antd';
+import {actions} from '../../../reducers/user_group/user_group_list'
+import {UserGroupCell} from './components/UserGroupCell';
 import style from './style.css'
 
 const {get_all_user_groups, edit_user_group} = actions;
@@ -16,72 +17,27 @@ class UserListGroup extends Component {
     }
 
     render() {
-        return (
-            const columns = [{
-				title:'名字',
-				dataIndex:'name',
-				key:'name',
-				width: 100
-			}, 
-			{
-				title:'描述',
-				dataIndex:'description',
-				key:'description',
-				width: 100
-			}, 
-			{
-				title: '操作',
-				key: 'action',
-				width: 100,
-				render: (text, record) => (	
-					 <UserGroupCell
-						edit_user_group={(id)=>this.props.edit_user_group(id)}
-						history={this.props.history}
-						get_user_group={(id)=>this.props.get_user_group(id)}
-						delete={(id)=>this.props.delete_user_group(id)}
-						key={index} 
-						data={item}
-					/>
-				)
-			}];
+		const columns = [{
+			title:'名称',
+			dataIndex:'name',
+			key:'name',
+			width: 100
+		}, 
+		{
+			title: '操作',
+			key: 'action',
+			width: 100
+		}];
 
-			return (	
-				<div>
-				  <Row className={style.titleRow}>
-					<Col span={12}><h2>客户组管理</h2></Col>
-					<Col span={12}><Button type="primary" icon="plus" className={style.btnAdd} onClick={()=>{this.props.history.push('/user_group/add')}}/></Col>
-				  </Row>
-				  <Table rowKey="name" columns={columns} dataSource={this.props.customerList} />
-				</div>
-			)
-			
-			
-			<!--<div>
-                <h2>用户组管理</h2>
-				<div className={style.userGroupListContainer}>
-                    {
-                        this.props.list.map((item,index)=>(
-                            <UserGroupCell
-                                edit_user_group={(id)=>this.props.edit_user_group(id)}
-                                history={this.props.history}
-                                getUserGroupDetail={(id)=>this.props.get_user_group_detail(id)}
-                                delete={(id)=>this.props.delete_user_group(id)}
-                                key={index} 
-								data={item}
-							/>
-                        ))
-                    }
-                </div>
-                <div>
-                    <Pagination
-                        onChange={(pageNum)=>{
-                            this.props.get_user_groups(pageNum);
-                        }}
-                        current={this.props.pageNum}
-                        total={this.props.total}/>
-                </div>
-            </div>-->
-        )
+		return (	
+			<div>
+			  <Row className={style.titleRow}>
+				<Col span={12}><h2>客户组管理</h2></Col>
+				<Col span={12}><Button type='primary' icon='plus' className={style.btnAdd} onClick={()=>{this.props.history.push('/user_group/add')}}/></Col>
+			  </Row>
+			  <Table rowKey='name' columns={columns} dataSource={this.props.list} />
+			</div>
+		)
     }
 
     componentDidMount() {
@@ -103,7 +59,7 @@ UserListGroup.defaultProps = {
 };
 
 function mapStateToProps(state) {
-    let {pageNum, list,total} = state.admin.user_groups;
+    let {pageNum, list, total} = state.admin.user_groups;
     return {
         pageNum,
         list,
